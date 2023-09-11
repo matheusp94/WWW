7f8b098769d4ab5e717365cc2bc66eb6e0cbf78d
matheusp94 <matheus.p940@gmail.com>
2023-09-11 20:42:29 -0300

    commit 2

--- projetoporto/src/components/header/header.js
+++ projetoporto/src/components/header/header.js
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
-export default function Header(){
-    return(
+export default function Header() {
+    return (
 
         <header>
             <h1>Challenge Porto</h1>
             <img src="logo.png" alt="Logo" id="logo"></img>
             < nav>
                 <ul>
-                    <li><Link href="/index">Página inicial</Link></li>
+                    <li><Link href="/">Página inicial</Link></li>
                     <li><Link href="/ideia">Nossa Ideia</Link></li>
                     <li><Link href="/organograma">Organograma</Link></li>
                     <li><Link href="/equipe">Equipe</Link></li>
